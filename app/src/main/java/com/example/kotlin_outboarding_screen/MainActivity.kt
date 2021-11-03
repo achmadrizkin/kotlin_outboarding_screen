b0f97650a35b607daac2f2803bb672a126d4bc06
achmadrizkin <arizki.nf02@gmail.com>
2021-11-03 16:57:54 +0700

    Add feature next button

--- app/src/main/java/com/example/kotlin_outboarding_screen/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_outboarding_screen/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin_outboarding_screen
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.kotlin_outboarding_screen.adapter.OnBoardingViewPagerAdapter
@@ -11,12 +12,15 @@ class MainActivity : AppCompatActivity() {
     var onBoardingViewPagerAdapter: OnBoardingViewPagerAdapter? = null
     var tabLayout: TabLayout? = null
     var onBoardingViewPager: ViewPager? = null
+    var next: TextView? = null
+    var position = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tabLayout = findViewById(R.id.tab_indicator)
+        next = findViewById(R.id.next)
 
         // add data to model class
         val onBoardingData: MutableList<OnBoardingData> = ArrayList()
@@ -25,6 +29,34 @@ class MainActivity : AppCompatActivity() {
         onBoardingData.add(OnBoardingData("Keep notes", "Lorem ipsum dolor sit amet\nconsectetur adipiscing elit.", R.drawable.notes3))
 
         setOnBoardingViewPagerAdapter(onBoardingData)
+        position = onBoardingViewPager!!.currentItem
+
+        //
+        next?.setOnClickListener {
+            if (position < onBoardingData.size) {
+                position++
+                onBoardingViewPager!!.currentItem = position
+            }
+        }
+
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                position = tab!!.position
+                if (tab.position == onBoardingData.size - 1) {
+                    next!!.text == "Get Started"
+                } else {
+                    next!!.text == "Next"
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
 
     }
 
